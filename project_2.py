bafb2ed7efc04ef6bec6410fe98b2e1bbb262221
JanaVesela <Janca.Vesela90@gmail.com>
2025-02-02 21:29:31 +0100

    second_project

--- project_2.py
+++ project_2.py
@@ -21,45 +21,27 @@ import time
 
 # separator
 separator = 70 * "-"
-
-#program pozdraví užitele a vypíše úvodní text
-print("Hi there!")
-
-# separator
-print(separator)
-
-print("I've generated a random 4 digit number for you.")
-print("Let's play a bulls and cows game.")
+def welcomeMessage():
+    print("Hi there!")#program pozdraví užitele a vypíše úvodní text
+    print(separator)# separator
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
 
 # separator
 print(separator)
 
 #vytvoří tajné 4místné číslo
-while True:
-    secret_number = random.randint(1000, 9999)
-    if len(set(str(secret_number))) == 4:
-        break
-  
-time_start = time.time()
-user_number = 0
+def generate_secret_number():
+    while True:
+        secret_number = random.randint(1000, 9999)
+        if len(set(str(secret_number))) == 4:
+            return secret_number
+            
 
-while user_number!= secret_number:
-    user_number = input("Enter a number")
+def round_evaluation(secret_number, user_number):
     bulls = 0
     cows = 0
-    if len(user_number) !=4 :#nesmí zadt číslo kratší nebo delší než 4 čísla
-        print("The number must have 4 digitis")
-        continue
-    if not user_number.isnumeric():#nesmí obsahovat nečíselné znaky
-        print("Pleas put the number")
-        continue
-    if user_number.startswith ("0"):#nesmí začínat 0
-        print("Sorry, the number not start whit 0")
-        continue
-    if len(set(user_number)) != 4:#nesmí obsahovat duplicity
-        print("Input contains duplicate digits.")
-        continue
-
+  
     number_find = (user_number.find(str(secret_number)[0]))
     if number_find == 0:
         bulls += 1
@@ -81,8 +63,50 @@ while user_number!= secret_number:
     elif number_find > -1:
         cows += 1
 
-time_stop = time.time()
-finish_time = time_stop - time_start
-seconds = finish_time / 60
-print("Your total time is", {seconds}, "seconds.")
+    return bulls, cows ()
+        
+    
 
+def round_info (bulls, cows):
+    bull_text = "bull"
+    cow_text = "cow"
+    if bulls > 1:
+        bull_text = "bulls"
+
+    if cows > 1:
+        cow_text = "cows"
+
+    print(f"{bulls} {bull_text}, {cows} {cow_text}\n{separator}")
+
+def validate_guess (user_guess):
+    return len(user_guess) != 4 or not user_guess.isdigit() or user_guess[0] == '0' or len(set(user_guess)) != 4
+
+def game_results (atempts, time_start):
+    time_stop = time.time()
+    finish_time = time_stop - time_start
+    seconds = finish_time / 60
+    print("Your total time is", {seconds}, "seconds.")
+    print("You've guessed the right number for, {atempts}, atempts.")
+
+def main():
+    welcomeMessage()
+
+    secret_number = generate_secret_number ()
+    atempts = 0
+    time_start = time.time()
+
+    while True:
+        user_guess = input("Enter a number")
+        atempts +=1
+        if validate_guess (user_guess):
+            print("Invalid input. Please enter 4 unique digits that don't start with 0.")
+            continue
+        bulls, cows = round_evaluation(secret_number, user_guess)
+        round_info(bulls, cows)
+        if bulls == 4:
+            break
+
+    game_results(atempts, time_start)
+
+if __name__ == "__main__":
+    main()
