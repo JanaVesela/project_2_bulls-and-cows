79d288a08fd838d214b4e8dcd5e1c9e4806075fd
JanaVesela <Janca.Vesela90@gmail.com>
2025-02-04 20:33:01 +0100

    second project

--- project_2.py
+++ project_2.py
@@ -42,17 +42,14 @@ def generate_secret_number():
 def round_evaluation(secret_number, user_number):
     bulls = 0
     cows = 0
-    for x in renge(4):
+    for x in range(4):
         number_find = (user_number.find(str(secret_number)[x]))
-    if number_find == x:
-        bulls += 1
-    elif number_find > -1:
-        cows += 1
+        if number_find == x:
+            bulls += 1
+        elif number_find > -1:
+            cows += 1
     
     return bulls, cows
-
-
-separator = /
 
 def round_info (bulls, cows):
     """   
@@ -66,7 +63,7 @@ def round_info (bulls, cows):
     if cows > 1:
         cow_text = "cows"
 
-    print(f"{bulls} {bull_text}, {cows} {cow_text}{'separator'}")
+    print(f"{bulls} {bull_text}, {cows} {cow_text}")
 
 def validate_guess (user_guess):
     """
@@ -82,10 +79,20 @@ def game_results (atempts, time_start):
     time_stop = time.time()
     finish_time = time_stop - time_start
     seconds = finish_time / 60
-    print(f"Your total time is", {seconds}, "seconds.")
-    print(f"You've guessed the right number for", {atempts}, "atempts.")
+    print(f"Your total time is {seconds} seconds.")
+    print(f"You've guessed the right number for {atempts} atempts.")
 
 def main():
+    """
+    zde uživatel hádá čtyřciferná čísla a podle úspěšnosti (uhádnutí správné
+    číslice nebo její pozice) dostává zpětnou vazbu ve formě bodového ohodnocení. 
+    "bulls" (býci), pokud uživatel uhodne jak číslo, tak jeho umístění 
+    "cows" (krávy), pokud uživatel uhodne pouze číslo, ale ne jeho umístění
+
+    spustí se počítání času a počítání pokusů
+
+    když uživatel číslo uhodne ukáží se mu výsledky
+    """
     welcome_message()
 
     secret_number = generate_secret_number ()
